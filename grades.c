054cdf8e78de7ab4f26318c7b5640d3c5bf8330f
Dakshayani <80052751+DakshayaniSirigudi@users.noreply.github.com>
2021-03-14 11:26:36 +0530

    Create grades.c
    This file tells the grade of the student based on the marks entered.

---
+++ grades.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+ int i=0;
+ printf("enter the marks of the kid");
+ scanf("%d",i);
+ if(i>=85&&i<=100)
+ {
+  printf("Grade is A");
+  elseif(i>=70)
+  {
+   printf("Grade is B");
+   elseif(i>=55)
+   {
+    printf("Grade is C");
+    elseif(i>=40)
+    {
+     printf("Grade is D");
+     else(i<40)
+     {
+      printf("Grade is F");
+     }
+    }
+   }
+  }
+ }
+ return 0;
+}
+  
+   
